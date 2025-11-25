8e51e6540d33226bd6cbdfc3ef2a71d30f693002
nikitafedorov008 <nikitafedorov008@gmail.com>
2025-11-25 19:29:10 +0300

    Updated README.md

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'screens/reminders_screen.dart';
 
 void main() async {
   // Инициализация .env файла
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   runApp(const HealthAssistantApp());
 }
 
